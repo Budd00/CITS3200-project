500d1549295f91debfae77afd4ff7383ec0ee557
Smiley260 <smiley2600@gmail.com>
2020-08-29 11:38:15 +0800

    extra helper funcs added, search func complete

--- webapp/libapp/models.py
+++ webapp/libapp/models.py
@@ -95,23 +95,64 @@ def link_asset(asset, tag):
     newEdge.save()
     return
 
-#returns a list of assets with direct links to this tag
-def find_assets_direct(tag):
+#returns a list of assets with direct links to this tag, ignoring any assets in the found list
+def find_assets_direct(tag, found):
+    #empty list to store found assets
     assets= []
+    #find all edges linking an asset to the given tag
     asset_query = AssetEdge.objects.filter(tag_id__exact=tag.id)
+    #for each edge found, retrieve the asset
     for link in asset_query:
         this_asset = Asset.objects.filter(id__exact = link.asset_id)[0]
-        assets.append(this_asset)
+        #if the found asset is nt in the "found" list, add it to the assets list
+        if this_asset not in found:
+            assets.append(this_asset)
+    #return the assets list
     return assets
 
-#returns a list of tags that imply the given tag
+#returns a list of tags with direct links to this asset, ignoring any tags in the ignore list
+def find_assets_direct(asset, ignore):
+    #empty list to store found tags
+    tags= []
+    #find all edges linking a tag to the given asset
+    tag_query = AssetEdge.objects.filter(asset_id__exact=asset.id)
+    #for each edge found, retrieve the tag
+    for link in tag_query:
+        this_tag = Tag.objects.filter(id__exact = link.tag_id)[0]
+        #if the found tag is not in the ignore list, add it to the tags list
+        if this_tag not in ignore:
+            assets.append(this_tag)
+    #return the tags list
+    return assets
+
+#returns a list of tags that imply the given tag, ignoring any tags in the ignore list
 def find_parent_tags(tag, ignore):
+    #empty tags list to store found parent tags
     tags = []
+    #find all edges with the given tag as the child tag
     tag_query = Edge.objects.filter(child_tag__exact = tag.id)
+    #for each edge found, retreive the parent tag
     for link in tag_query:
         this_tag = Tag.objects.filter(id__exact = link.parent_tag)[0]
+        #if the parent tag is not in the ignore list, add it to the tags list
         if this_tag.id not in ignore:
             tags.append(this_tag)
+    #return the tags list
+    return tags
+
+    #returns a list of tags that the given tag implies, ignoring any tags in the ignore list
+def find_parent_tags(tag, ignore):
+    #empty tags list to store found child tags
+    tags = []
+    #find all edges with the given tag as the parent tag
+    tag_query = Edge.objects.filter(parent_tag__exact = tag.id)
+    #for each edge found, retreive the child tag
+    for link in tag_query:
+        this_tag = Tag.objects.filter(id__exact = link.child_tag)[0]
+        #if the child tag is not in the ignore list, add it to the tags list
+        if this_tag.id not in ignore:
+            tags.append(this_tag)
+    #return the tags list
     return tags
 
 #returns a list of assets linked to the given tag either directly or implicitly
@@ -136,6 +177,8 @@ def find_all_assets(tag):
         tags_to_check = tags_to_check+parents
 
         #find all assets directly linked to the current tag
-        these_assets = find_assets_direct(checking)
+        these_assets = find_assets_direct(checking, assets)
         #add found assets to the list of assets
         assets = assets + these_assets
+    #return the list of found assets
+    return assets

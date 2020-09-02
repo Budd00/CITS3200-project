2dee20b96e408dc6e11f8c277f595921f5d990e7
Xinhao <1935228697@qq.com>
2020-09-02 16:15:02 +0800

    membership db functional(foreign key)
    basic membership db functional, testmodel folder taken out. Three basic functions add(contained in memdb.py). Foreign Key to be added.

--- webapp/webapp/webapp/settings.py
+++ webapp/webapp/webapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'membership_db',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER':'root',
+        'PASSWORD': 'hao1999.',
     }
 }
 

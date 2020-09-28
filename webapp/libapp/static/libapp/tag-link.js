ce6e0d2c28308b2612c1e0b879fb52d9c84e1a4e
mazza <msyukri.zaidin@gmail.com>
2020-09-28 16:03:34 +0800

    Implement hierarchical tag model

---
+++ webapp/libapp/static/libapp/tag-link.js
@@ -0,0 +1,18 @@
+
+//Used for finding the direct parent of the currently selected tag
+function find_parent(input_id) {
+    //This whole string of parentNodes gives the tag id of the direct parent
+    let parent_tag = input_id.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.getAttribute("value")
+    let parent_tag_input = input_id
+    //For loop is used to find the input field of the parent tag
+    for(let i = 0; i < parent_tag_input.children.length; i++) {
+        if(parent_tag_input.children[i].name =='parent_tag') {
+            parent_tag_input = parent_tag_input.children[i]
+        }
+    }
+    //Value of the parent tag input field is initialised so the Django back-end can read the data when the form is submitted
+    parent_tag_input.value = parent_tag
+    return true;
+}
+
+

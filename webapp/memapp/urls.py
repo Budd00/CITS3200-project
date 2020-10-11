5fbfc0639fa825986cf58a3332929e5a1a3c97ff
Xinhao <1935228697@qq.com>
2020-10-11 16:37:54 +0800

    Authentication refine
    Added more authentication check

--- webapp/memapp/urls.py
+++ webapp/memapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-
-from . import views
+from django.conf.urls import url
+from . import views, memdb
 
 urlpatterns = [
-    path('', views.index, name='index'),
+   path('', views.IndexView.as_view(), name='index'),
+    path('add_info', views.mod_memdb),
+    path('add_detail', views.add_detail),
+    path('getinfo/<int:m_id>', views.get_membership_data, name="get_info"),
+    path('getdetail/<int:m_id>', views.get_detail_data, name="get_detail"),
+    # path('login',views.my_view),
+    path('create', views.create_user),
 ]
+
